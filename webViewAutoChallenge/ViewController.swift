4032bed2f7ed26290cc93cab8146c963d8b4527d
Philip Nduka <philip.igboba@mkobo.com.ng>
2023-07-16 12:56:49 +0100

    Initial Commit

---
+++ webViewAutoChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  webViewAutoChallenge
+//
+//  Created by APPLE on 7/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
